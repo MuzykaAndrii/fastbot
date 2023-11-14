c8eee1b06556c52f1ef611da74b88769e98af58c
MuzykaAndrii <andrymyzik@gmail.com>
2023-11-14 17:11:31 +0200

    deleted native handlers for bot api

--- app/main.py
+++ app/main.py
@@ -2,21 +2,12 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.bot.router import router as bot_router
-from app.bot.services.webhook import BotWebhookService
 from app.config import settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # on startup
-
-    bot_webhook_service = BotWebhookService(
-        bot_token=settings.BOT_TOKEN,
-        webhook_url=settings.WEBHOOK_URL,
-        webhook_prefix=bot_router.prefix,
-    )
-    bot_webhook_service.set_webhook_if_current_unmatched()
 
     yield
 
@@ -27,5 +18,3 @@ app = FastAPI(
     debug=settings.DEBUG,
     lifespan=lifespan,
 )
-
-app.include_router(bot_router)
