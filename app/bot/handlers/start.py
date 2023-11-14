e91130b07f158c53eaf57baf55d97c2d29fce3e8
MuzykaAndrii <andrymyzik@gmail.com>
2023-11-14 17:39:40 +0200

    implemented echo handler

--- app/bot/handlers/start.py
+++ app/bot/handlers/start.py
@@ -8,3 +8,8 @@ router = Router()
 @router.message(Command('start'))
 async def start_handler(message: types.Message):
     await message.answer(f"Hey {message.from_user.first_name}")
+
+
+@router.message()
+async def echo_handler(message: types.Message):
+    await message.answer(message.text)
