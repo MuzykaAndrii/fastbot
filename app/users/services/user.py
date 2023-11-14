38c45a67f42abaf42ae505425ef02507791458a5
MuzykaAndrii <andrymyzik@gmail.com>
2023-11-14 22:10:41 +0200

    implemented get_or_create_from_tg_id service

---
+++ app/users/services/user.py
@@ -0,0 +1,13 @@
+
+
+from app.users.dal import UserDAL
+from app.users.models import User
+
+
+class UserService:
+
+    @staticmethod
+    async def get_or_create_from_tg_id(tg_id: int) -> User:
+        user = await UserDAL.get_or_create(ig_id=tg_id)
+
+        return user
