0a2fbb86e69549caae3c54040a66a79f4c58d95e
MuzykaAndrii <andrymyzik@gmail.com>
2023-11-15 15:15:45 +0200

    created service for getting user from tg id

--- app/users/services/user.py
+++ app/users/services/user.py
@@ -5,7 +5,12 @@ from app.users.models import User
 class UserService:
 
     @staticmethod
-    async def get_or_create_from_tg_id(tg_id: int) -> User:
+    async def get_or_create_by_tg_id(tg_id: int) -> User:
         user = await UserDAL.get_or_create(tg_id=tg_id)
 
+        return user
+    
+    @staticmethod
+    async def get_by_tg_id(tg_id: int) -> User:
+        user = await UserDAL.get_one(tg_id=tg_id)
         return user
