850fdd4d65e9dfa5e7c9975ece9a5452040bca1b
MuzykaAndrii <andrymyzik@gmail.com>
2023-11-18 19:30:30 +0200

    updated select strategy keyboard

--- app/bot/vocabulary/keyboards.py
+++ app/bot/vocabulary/keyboards.py
@@ -5,11 +5,16 @@ from aiogram.types import (
 
 
 def get_select_strategy_keyboard():
-    btn_bulk = KeyboardButton(text="Bulk")
-    btn_line_by_line = KeyboardButton(text="Line by line")
+    btn_bulk = KeyboardButton(text="Bulk 📝")
+    btn_line_by_line = KeyboardButton(text="Line by line 🏷️")
+    btn_cancel = KeyboardButton(text="Cancel")
 
-    select_strategy_keyboard = ReplyKeyboardMarkup(keyboard=[
-        [btn_bulk, btn_line_by_line]
-    ])
+    select_strategy_keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [btn_bulk, btn_line_by_line],
+            [btn_cancel],
+        ],
+        resize_keyboard=True,
+    )
 
     return select_strategy_keyboard
