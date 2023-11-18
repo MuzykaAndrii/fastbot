7b278e04536022767ffd2872b9f6378c88f1d9b6
MuzykaAndrii <andrymyzik@gmail.com>
2023-11-18 19:38:16 +0200

    fixed keyboard

--- app/bot/vocabulary/keyboards.py
+++ app/bot/vocabulary/keyboards.py
@@ -5,8 +5,8 @@ from aiogram.types import (
 
 
 def get_select_strategy_keyboard():
-    btn_bulk = KeyboardButton(text="Bulk 📝")
-    btn_line_by_line = KeyboardButton(text="Line by line 🏷️")
+    btn_bulk = KeyboardButton(text="Bulk")
+    btn_line_by_line = KeyboardButton(text="Line by line")
     btn_cancel = KeyboardButton(text="Cancel")
 
     select_strategy_keyboard = ReplyKeyboardMarkup(
