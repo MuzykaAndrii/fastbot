0da8b6ba6f7ad2107869405aacaf4d8a5bbb23c7
MuzykaAndrii <andrymyzik@gmail.com>
2023-11-23 19:55:34 +0200

    refactored chat action while sending vocabularies

--- app/bot/vocabulary/handlers/show_vocabulary.py
+++ app/bot/vocabulary/handlers/show_vocabulary.py
@@ -1,6 +1,6 @@
 from aiogram.filters import Command
 from aiogram import Router, types
-from aiogram.enums import ChatAction
+from aiogram.utils.chat_action import ChatActionSender
 
 from app.bot.vocabulary.keyboards import get_vocabulary_actions_keyboard
 from app.bot.vocabulary.messages import VocabularyMessages
@@ -12,19 +12,15 @@ router = Router()
 
 @router.message(Command("my"))
 async def show_vocabularies(message: types.Message):
-    await message.bot.send_chat_action(
-        chat_id=message.chat.id,
-        action=ChatAction.TYPING,
-    )
+    async with ChatActionSender.typing(chat_id=message.chat.id, bot=message.bot):
+        vocabulary_sets = await VocabularyService.get_user_vocabularies(message.from_user.id)
 
-    vocabulary_sets = await VocabularyService.get_user_vocabularies(message.from_user.id)
+        if not vocabulary_sets:
+            await message.answer(VocabularyMessages.user_havent_any_vocabularies)
+            return
+        
+        for vocabulary_set in vocabulary_sets:  # type: VocabularySetSchema
+            vocabulary_set_msg = VocabularyMessages.get_vocabulary_entity_msg(vocabulary_set)
+            vocabulary_actions_keyboard = get_vocabulary_actions_keyboard(vocabulary_set.id)
 
-    if not vocabulary_sets:
-        await message.answer(VocabularyMessages.user_havent_any_vocabularies)
-        return
-
-    for vocabulary_set in vocabulary_sets:  # type: VocabularySetSchema
-        vocabulary_set_msg = VocabularyMessages.get_vocabulary_entity_msg(vocabulary_set)
-
-        vocabulary_actions_keyboard = get_vocabulary_actions_keyboard(vocabulary_set.id)
-        await message.answer(vocabulary_set_msg, reply_markup=vocabulary_actions_keyboard)
+            await message.answer(vocabulary_set_msg, reply_markup=vocabulary_actions_keyboard)
