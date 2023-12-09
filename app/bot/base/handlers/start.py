608b3bc53a6a51a04a75a4ce6cec861438bd16af
MuzykaAndrii <andrymyzik@gmail.com>
2023-12-09 23:16:33 +0200

    created unknown command msg handler

--- app/bot/base/handlers/start.py
+++ app/bot/base/handlers/start.py
@@ -1,5 +1,6 @@
 from aiogram import types, Router
 from aiogram.filters import CommandStart
+from app.bot.base.messages import BaseMessages
 
 from app.users.services.user import UserService
 
@@ -9,7 +10,11 @@ router = Router()
 
 @router.message(CommandStart())
 async def start_handler(message: types.Message):
-    current_user_tg_id = message.from_user.id
-    user = await UserService.get_or_create_by_tg_id(current_user_tg_id)
+    await UserService.get_or_create_by_tg_id(message.from_user.id)
 
-    await message.answer(f"Hey {message.from_user.first_name}")
+    await message.answer(BaseMessages.start_msg.format(username=message.from_user.username))
+
+
+@router.message()
+async def unknown_command_handler(message: types.Message):
+    await message.answer(BaseMessages.unknown_command_msg)
---
+++ app/bot/base/messages.py
@@ -0,0 +1,5 @@
+
+
+class BaseMessages:
+    start_msg = "Hey, {username}"
+    unknown_command_msg = "I dont get what u mean🤔\nChoose command from menu below⬇️"
