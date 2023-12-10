1d9040ceb55626dbdd818644478d801550316419
MuzykaAndrii <andrymyzik@gmail.com>
2023-12-10 18:46:05 +0200

    implemented advanced answer checker for quiz

--- app/bot/vocabulary/validators.py
+++ app/bot/vocabulary/validators.py
@@ -1,4 +1,5 @@
 import re
+from difflib import SequenceMatcher
 
 
 class VocabularyValidator:
@@ -22,3 +23,49 @@ class VocabularyValidator:
             return False
         else:
             return True
+
+
+class QuizAnswerChecker:
+    def __init__(
+            self,
+            suggested_translation: str,
+            correct_translation: str,
+            similarity_ratio_treshold: float = .95,
+            variants_separator: str = ","
+    ) -> None:
+        self.suggested_translation = suggested_translation.strip().lower()
+        self.correct_translation = self._trim_parenthesis(correct_translation).strip().lower()
+        self._similarity_ratio_treshold = similarity_ratio_treshold
+        self._variants_separator = variants_separator
+
+    def _get_correct_translation_variants(self) -> list[str]:
+        return re.split(r"\s*,\s*", self.correct_translation)
+
+
+    def _is_suggested_translation_match(self, suggested_translation: str, correct_translation: str) -> bool:
+        similarity = SequenceMatcher(None, suggested_translation, correct_translation).ratio()
+
+        return similarity >= self._similarity_ratio_treshold
+
+    def _trim_parenthesis(self, text: str) -> str:
+        pattern = r'\([^)]*\)'
+        return re.sub(pattern, '', text)
+    
+    def check_full_similarity(self) -> bool:
+        return self._is_suggested_translation_match(self.suggested_translation, self.correct_translation)
+    
+    def check_partial_similarity(self) -> bool:
+        correct_translation_variants = self._get_correct_translation_variants()
+
+        for variant in correct_translation_variants:
+            if self._is_suggested_translation_match(self.suggested_translation, variant):
+                return True
+        return False
+
+
+    def check_correctness(self) -> bool:
+        if self._variants_separator in self.suggested_translation:
+            return self.check_full_similarity()
+        else:
+            return self.check_partial_similarity()
+    
