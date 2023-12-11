14ae329580974cbb8e3f33fb031dd749913a2db3
MuzykaAndrii <andrymyzik@gmail.com>
2023-12-11 16:50:42 +0200

    refactored quiz scene

--- app/bot/vocabulary/handlers/quiz_vocabulary.py
+++ app/bot/vocabulary/handlers/quiz_vocabulary.py
@@ -1,7 +1,7 @@
 import random
 
 from aiogram import F, Router
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, ReplyKeyboardRemove
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.scene import Scene, on
 
@@ -26,10 +26,10 @@ class QuizScene(Scene, state="quiz"):
         vocabulary = await VocabularyService.get_vocabulary(query.from_user.id, callback_data.vocabulary_id)
         random.shuffle(vocabulary.language_pairs)
         await state.update_data(
-            step=0,
             language_pairs=vocabulary.language_pairs,
             correct_answers_count=0,
-            wrong_answers_count=0
+            wrong_answers_count=0,
+            questions_count=len(vocabulary.language_pairs),
         )
 
         await self.ask(query.message, state)
@@ -37,54 +37,51 @@ class QuizScene(Scene, state="quiz"):
 
     async def ask(self, message: Message, state: FSMContext):
         state_data = await state.get_data()
-        step = state_data.get("step")
         language_pairs = state_data.get("language_pairs")
 
         try:
-            question_item = language_pairs[step]
-        except (IndexError, TypeError):
+            question_item = language_pairs.pop()
+        except IndexError:
             return await self.wizard.exit(show_stats=True)
 
         last_question_msg = await message.answer(
             VocabularyMessages.quiz_question.format(word=question_item.translation)
         )
-        await state.update_data(step=step, last_question_msg=last_question_msg)
+        await state.update_data(last_question_msg=last_question_msg, current_language_pair=question_item)
     
 
     @on.message(F.text == "🚪 Leave quiz")
     async def leave_quiz(self, message: Message):
-        await message.answer(VocabularyMessages.leave_quiz)
+        await message.answer(VocabularyMessages.leave_quiz, reply_markup=ReplyKeyboardRemove())
         return await self.wizard.exit()
         
     
     @on.message(F.text)
     async def answer(self, message: Message, state: FSMContext) -> None:
         state_data = await state.get_data()
-        step = state_data.get("step")
         correct_answers_count = state_data.get("correct_answers_count")
         wrong_answers_count = state_data.get("wrong_answers_count")
         last_question_msg: Message = state_data.get("last_question_msg")
-        current_pair: LanguagePairSchema = state_data.get("language_pairs")[step]
+        current_language_pair: LanguagePairSchema = state_data.get("current_language_pair")
 
-        answer_checker = QuizAnswerChecker(message.text, current_pair.word)
+        answer_checker = QuizAnswerChecker(message.text, current_language_pair.word)
         if answer_checker.check_correctness():
             answer_response_msg = VocabularyMessages.quiz_success_answer.format(
                 word=message.text,
-                translation=current_pair.translation,
+                translation=current_language_pair.translation,
             )
             correct_answers_count += 1
         else:
             answer_response_msg = VocabularyMessages.quiz_wrong_answer.format(
-                word=current_pair.word,
-                translation=current_pair.translation,
+                word=current_language_pair.word,
+                translation=current_language_pair.translation,
                 suggestion=message.text
             )
             wrong_answers_count += 1
         
-        await message.delete()
         await last_question_msg.edit_text(answer_response_msg)
+        await message.delete()
         await state.update_data(
-            step=step+1,
             correct_answers_count=correct_answers_count,
             wrong_answers_count=wrong_answers_count
         )
@@ -107,17 +104,19 @@ class QuizScene(Scene, state="quiz"):
 
         correct_answers_count = state_data.get("correct_answers_count")
         wrong_answers_count = state_data.get("wrong_answers_count")
-        pairs: list[LanguagePairSchema] = state_data.get("language_pairs")
+        questions_count = state_data.get("questions_count")
 
-        questions_count = len(pairs)
         success_rate = round(correct_answers_count / questions_count * 100, 1)
 
-        await message.answer(VocabularyMessages.quiz_stats.format(
-            correct_guesses=correct_answers_count,
-            wrong_guesses=wrong_answers_count,
-            success_rate=success_rate,
-            total_words_attempted=questions_count,
-        ))
+        await message.answer(
+            VocabularyMessages.quiz_stats.format(
+                correct_guesses=correct_answers_count,
+                wrong_guesses=wrong_answers_count,
+                success_rate=success_rate,
+                total_words_attempted=questions_count,
+            ),
+            reply_markup=ReplyKeyboardRemove(),
+        )
 
 
 router = Router()
