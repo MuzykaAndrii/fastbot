fcb47ce912180e6d751f258aa9db1504c3a04295
MuzykaAndrii <andrymyzik@gmail.com>
2023-12-17 18:08:14 +0200

    fixed floating issue with bad request when updating quiz msg

--- app/bot/vocabulary/handlers/quiz_vocabulary.py
+++ app/bot/vocabulary/handlers/quiz_vocabulary.py
@@ -1,8 +1,11 @@
+from contextlib import suppress
+
 from aiogram import F, Router
 from aiogram.types import CallbackQuery, Message, ReplyKeyboardRemove
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.scene import Scene, on
 from aiogram.fsm.scene import SceneRegistry
+from aiogram.exceptions import TelegramBadRequest
 
 from app.bot.modules.base_quiz import Quiz
 from app.bot.vocabulary.callback_patterns import StartQuizCallbackData, VocabularyAction, VocabularyCallbackData
@@ -101,9 +104,10 @@ class QuizScene(Scene, state="quiz"):
                 suggestion=message.text
             )
             quiz.increment_wrong_answers_count()
-                
-        await quiz.last_question_msg.edit_text(answer_response_msg)
-        await message.delete()
+        
+        with suppress(TelegramBadRequest):
+            await quiz.last_question_msg.edit_text(answer_response_msg)
+            await message.delete()
 
         await quiz.save_to_state(state)
         await self.wizard.retake()
