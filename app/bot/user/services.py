974357e5f6f392cad3d7eac8f4aeb27d1994ab22
MuzykaAndrii <andrymyzik@gmail.com>
2023-12-17 18:01:14 +0200

    implemented skipping notifications when user have state

---
+++ app/bot/user/services.py
@@ -0,0 +1,28 @@
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.base import StorageKey
+
+from app.bot.main import bot
+
+
+class UserService:
+    def __init__(self, user_tg_id: int) -> None:
+        self.user_tg_id = user_tg_id
+
+    def get_user_context(self) -> FSMContext:
+        user_context = FSMContext(
+            storage=bot.dispatcher.storage,
+            key=StorageKey(
+                chat_id=self.user_tg_id,
+                user_id=self.user_tg_id,
+                bot_id=bot.bot.id,
+            )
+        )
+        return user_context
+
+    async def user_has_active_state(self) -> bool:
+        context = self.get_user_context()
+        state = await context.get_state()
+
+        if state is None:
+            return False
+        return True
