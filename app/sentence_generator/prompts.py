493a2008c1d862f57ac4fd5c84143197c538f095
MuzykaAndrii <andrymyzik@gmail.com>
2023-12-21 16:53:22 +0200

    implemented prompt for sentence generating

---
+++ app/sentence_generator/prompts.py
@@ -0,0 +1,5 @@
+
+
+class Prompts:
+    sentence_from_word = """Just provide me the ONE sentence with 15-30 words what contain KEYWORD: \"{word}\".
+    DO NOT INCLUDE ANY EXPLANATION, INTRODUCTORY STATEMENTS, ACCOMPANYING COMMENTS."""
