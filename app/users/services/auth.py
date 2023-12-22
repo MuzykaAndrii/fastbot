4da3239f2562946afcb4f60a42329a85bd43d8ec
MuzykaAndrii <andrymyzik@gmail.com>
2023-12-22 20:07:57 +0200

    impl authenticate user module

---
+++ app/users/services/auth.py
@@ -0,0 +1,21 @@
+from app.users.exceptions import UserInvalidPassword, UserNotFoundError
+from app.users.models import User
+from app.users.schemas import UserLogin
+from app.users.services.pwd import PWDService
+from app.users.services.user import UserService
+
+
+async def authenticate_user(user_in: UserLogin) -> User:
+    user = await UserService.get_by_email(user_in.username_or_email)
+
+    if not user:
+        raise UserNotFoundError
+
+    pass_matching = PWDService.verify_password(
+        raw_password=user_in.password, hashed_password=user.password_hash
+    )
+
+    if not pass_matching:
+        raise UserInvalidPassword
+
+    return user
