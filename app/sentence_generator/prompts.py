1f14497cef1bee0d9002fabfd11875cb7b230f74
MuzykaAndrii <andrymyzik@gmail.com>
2023-12-27 20:46:54 +0200

    fix deleted folders

--- app/sentence_generator/prompts.py
+++
@@ -1,5 +0,0 @@
-
-
-class Prompts:
-    sentence_from_word = """Just provide me the ONE simple B1 sentence with 5-10 words what contain KEYWORD: \"{word}\".
-    DO NOT INCLUDE ANY EXPLANATION, INTRODUCTORY STATEMENTS, ACCOMPANYING COMMENTS."""
