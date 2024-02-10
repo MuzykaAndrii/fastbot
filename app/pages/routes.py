6db750f7882aceefedd0c8103d4516f54714f613
MuzykaAndrii <andrymyzik@gmail.com>
2024-02-10 22:34:18 +0200

    impl: vocabulary rendering in dashboard

--- app/pages/routes.py
+++ app/pages/routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, HTTPException, Request
+from app.shared.exceptions import UserIsNotOwnerOfVocabulary, VocabularyDoesNotExist
 
 from app.users.models import User
 from app.auth.dependencies import get_current_user
+from app.vocabulary.services import VocabularyService
 from .template_engine import engine as template_engine
 
 
@@ -16,8 +18,16 @@ async def dashboard(
     vocabulary_id: int,
     user: User = Depends(get_current_user)
 ):
+    try:
+        vocabulary = await VocabularyService.get_vocabulary(user.id, vocabulary_id)
+    except VocabularyDoesNotExist:
+        raise HTTPException(404, "Vocabulary does not exist")
+    except UserIsNotOwnerOfVocabulary:
+        raise HTTPException(403, "Permission denied")
+    
+    vocabularies = await VocabularyService.get_all_user_vocabularies(user.id)
 
     return template_engine.TemplateResponse(
         name="dashboard.html",
-        context={"request": request},
+        context={"request": request, "current_vocabulary": vocabulary, "vocabularies": vocabularies},
     )
