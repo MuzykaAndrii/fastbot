5ca6ab0811860d89d16b61861a5f2dde9550a938
MuzykaAndrii <andrymyzik@gmail.com>
2024-02-10 21:00:57 +0200

    impl: authenticated user dependency in dashboard page

--- app/pages/routes.py
+++ app/pages/routes.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
 
+from app.users.models import User
+from app.auth.dependencies import get_current_user
 from .template_engine import engine as template_engine
 
 
@@ -9,7 +11,11 @@ router = APIRouter(
 )
 
 @router.get("/dashboard/{vocabulary_id}")
-async def dashboard(request: Request, vocabulary_id: int):
+async def dashboard(
+    request: Request,
+    vocabulary_id: int,
+    user: User = Depends(get_current_user)
+):
 
     return template_engine.TemplateResponse(
         name="dashboard.html",
