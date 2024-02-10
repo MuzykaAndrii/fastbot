017ecbcc40930e9d31c2780e28e1bcb144f7d39e
MuzykaAndrii <andrymyzik@gmail.com>
2024-02-10 20:51:29 +0200

    impl: dependency to get current user

---
+++ app/auth/dependencies.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, HTTPException, Request
+from app.jwt import Jwt
+from app.jwt.exceptions import JWTExpiredError, JwtNotValidError
+from app.users.dal import UserDAL
+
+from app.users.models import User
+from .cookie import AuthCookieManager
+
+
+def get_auth_token(request: Request) -> str:
+    auth_token = AuthCookieManager().get_cookie(request)
+
+    if not auth_token:
+        raise HTTPException(status_code=401)
+    return auth_token
+
+
+async def get_current_user(token: str = Depends(get_auth_token)) -> User:
+    try:
+        payload = Jwt.read_token(token)
+        user_id = int(payload.get("sub"))
+        user = await UserDAL.get_by_id(user_id)
+
+    except (JwtNotValidError, JWTExpiredError, ValueError):
+        raise HTTPException(status_code=401)
+
+    if not user:
+        raise HTTPException(status_code=401)
+
+    return user
