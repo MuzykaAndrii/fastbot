670117c1ff2623bc7473c45ab8735e0eab655912
MuzykaAndrii <andrymyzik@gmail.com>
2024-02-24 14:30:52 +0200

    refactor: added docstring to Base UOW

--- app/backend/db/unitofwork.py
+++ app/backend/db/unitofwork.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import TypeVar
+from typing import Self, TypeVar
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from app.backend.users.dal import UserDAL
@@ -32,11 +32,11 @@ class UnitOfWorkInterface(ABC):
 
 T = TypeVar("T", bound=BaseDAL)
 
-class AbstractUnitOfWork(UnitOfWorkInterface):
+class BaseUnitOfWork(UnitOfWorkInterface):
     def __init__(self, session_factory: async_sessionmaker) -> None:
         self._session_factory = session_factory
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> Self:
         self.session: AsyncSession = self._session_factory()
         self._init_repos()
 
@@ -44,6 +44,18 @@ class AbstractUnitOfWork(UnitOfWorkInterface):
 
     @abstractmethod
     def _init_repos(self) -> None:
+        """
+        Usage:
+            - Inherit BaseUnitOfWork and define this method
+            - use method _register_repo to attach db session to you're repositories
+        Example:
+            from your_repositories import User, Profile
+
+            class MyUnitOfWork(BaseUnitOfWork):
+                def _init_repos(self):
+                    self.users = self._register_repo(UserRepository)
+                    self.profiles = self._register_repo(ProfileRepository)
+        """
         pass
 
     def _register_repo(self, repo: type[BaseDAL]) -> BaseDAL:
@@ -62,17 +74,3 @@ class AbstractUnitOfWork(UnitOfWorkInterface):
 
     async def undo(self):
         await self.session.rollback()
-
-
-class UnitOfWork(AbstractUnitOfWork):
-    def _init_repos(self) -> None:
-        self.users = self._register_repo(UserDAL)
-        self.vocabularies = self._register_repo(VocabularySetDAL)
-        self.language_pairs = self._register_repo(LanguagePairDAL)
-
-
-if __name__ == '__main__':
-    from .session import async_session_maker
-
-    uow = UnitOfWork(async_session_maker)
-    uow.users.get_all()
