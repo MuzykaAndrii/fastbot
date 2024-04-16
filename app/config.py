452a3a6724727042c4e2ef5ec6b5ff678bd94d98
MuzykaAndrii <andrymyzik@gmail.com>
2024-04-16 16:11:15 +0300

    impl: ngrok tunnel for development

--- app/config.py
+++ app/config.py
@@ -17,7 +17,11 @@ ENV_FILE_PATH = BASE_DIR / ".env"
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=ENV_FILE_PATH, env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE_PATH,
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
 
     DEBUG: bool
     BOT_TOKEN: str
