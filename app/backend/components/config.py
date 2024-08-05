7ece0f4e40eb336660d5d8bc18e95e420bdcf116
MuzykaAndrii <andrymyzik@gmail.com>
2024-08-05 23:24:59 +0300

    refactor: divided environments to 3 types

--- app/backend/components/config.py
+++ app/backend/components/config.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from app.backend.db.config import DbSettings
@@ -5,10 +6,23 @@ from app.backend.auth.config import AuthSettings
 from app.backend.sentry.config import SentrySettings
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
-ENV_FILE_PATH = BASE_DIR / '.env'
+ENV_DIR = Path(__file__).resolve().parent.parent.parent.parent / "secrets" / "environment"
+PROD_ENV_FILE_PATH = ENV_DIR / '.env.prod'
+DEV_ENV_FILE_PATH = ENV_DIR / '.env.dev'
+TEST_ENV_FILE_PATH = ENV_DIR / '.env.test'
 
 
-db_settings = DbSettings(_env_file=ENV_FILE_PATH)
-auth_settings = AuthSettings(_env_file=ENV_FILE_PATH)
-sentry_settings = SentrySettings(_env_file=ENV_FILE_PATH)
+match os.getenv("MODE"):
+    case "PROD":
+        CURRENT_ENV_FILE_PATH = PROD_ENV_FILE_PATH
+    case "DEV":
+        CURRENT_ENV_FILE_PATH = DEV_ENV_FILE_PATH
+    case "TEST":
+        CURRENT_ENV_FILE_PATH = TEST_ENV_FILE_PATH
+    case _:
+        raise RuntimeError("Unknown environment file specified")
+
+
+db_settings = DbSettings(_env_file=CURRENT_ENV_FILE_PATH)
+auth_settings = AuthSettings(_env_file=CURRENT_ENV_FILE_PATH)
+sentry_settings = SentrySettings(_env_file=CURRENT_ENV_FILE_PATH)
