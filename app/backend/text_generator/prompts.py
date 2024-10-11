c853eaf0a639c619cc286322e8c9aae4657d498d
MuzykaAndrii <andrymyzik@gmail.com>
2024-10-11 23:28:10 +0300

    impl: text generator method: get_sentence_from_two_keywords

--- app/backend/text_generator/prompts.py
+++ app/backend/text_generator/prompts.py
@@ -4,6 +4,11 @@ sentence_from_word = """Just provide me the ONE simple B1 sentence with 5-10 wor
 DO NOT INCLUDE ANY EXPLANATION, INTRODUCTORY STATEMENTS, ACCOMPANYING COMMENTS. Wrap used keyword by: <u>word here</u>"""
 
 
+sentence_from_two_words = """Just provide me the ONE simple B1 sentence with 5-10 words what contain PRIMARY KEYWORD: \"{primary_word}\" and SECONDARY KEYWORD \"{secondary_word}\".
+DO NOT INCLUDE ANY EXPLANATION, INTRODUCTORY STATEMENTS, ACCOMPANYING COMMENTS.
+Wrap used PRIMARY KEYWORD by: <u>keyword here</u> and SECONDARY KEYWORD by: <i>keyword here</i>"""
+
+
 text_from_words = """Provide a very simple short story (up to {text_length} sentences) that contain the following words: \"{keywords}\".
 Wrap each keyword by: <u>word here</u>"""
 
--- app/backend/text_generator/text_generator.py
+++ app/backend/text_generator/text_generator.py
@@ -17,6 +17,20 @@ class TextGenerator:
         else:
             return sentence
     
+    async def get_sentence_from_two_keywords(
+        self,
+        primary_word: str,
+        secondary_word: str,
+        format: bool = True
+    ) -> str | None:
+        prompt = prompts.sentence_from_word.format(primary_word=primary_word, secondary_word=secondary_word)
+        sentence = await self.gpt.get_answer(prompt)
+        
+        if format and sentence is not None:
+            return self._format_answer(sentence)
+        else:
+            return sentence
+    
     async def get_text_from_keywords(self, *keywords) -> str | None:
         # TODO: add randomness to text length calculating
         prompt = prompts.text_from_words.format(
