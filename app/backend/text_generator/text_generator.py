65675e9eab72f71f1f77041484be7b8a12daf415
MuzykaAndrii <andrymyzik@gmail.com>
2024-10-16 18:23:25 +0300

    refactor: deleted redundant text generator formatting due to better prompts

--- app/backend/text_generator/text_generator.py
+++ app/backend/text_generator/text_generator.py
@@ -8,28 +8,13 @@ class TextGenerator:
     def __init__(self, gpt: GPT) -> None:
         self.gpt = gpt
 
-    async def get_sentence_from_keyword(self, keyword: str, format: bool = True) -> str | None:
+    async def get_sentence_from_keyword(self, keyword: str) -> str | None:
         prompt = prompts.sentence_from_word.format(word=keyword)
-        sentence = await self.gpt.get_answer(prompt)
-        
-        if format and sentence is not None:
-            return self._format_answer(sentence)
-        else:
-            return sentence
+        return await self.gpt.get_answer(prompt)
     
-    async def get_sentence_from_two_keywords(
-        self,
-        primary_word: str,
-        secondary_word: str,
-        format: bool = True
-    ) -> str | None:
+    async def get_sentence_from_two_keywords(self, primary_word: str, secondary_word: str) -> str | None:
         prompt = prompts.sentence_from_two_words.format(primary_word=primary_word, secondary_word=secondary_word)
-        sentence = await self.gpt.get_answer(prompt)
-        
-        if format and sentence is not None:
-            return self._format_answer(sentence)
-        else:
-            return sentence
+        return await self.gpt.get_answer(prompt)
     
     async def get_text_from_keywords(self, keywords: Iterable[str]) -> str | None:
         # TODO: add randomness to text length calculating
@@ -37,11 +22,5 @@ class TextGenerator:
             text_length=len(keywords) // 4,
             keywords=", ".join(keywords),
         )
-        text = await self.gpt.get_answer(prompt)
-        return text
 
-    def _format_answer(self, sentence: str) -> str:
-        sentence = sentence.split("\n")[-1]
-        if ":" in sentence:
-            return sentence.split(":")[-1]
-        return sentence
+        return await self.gpt.get_answer(prompt)
